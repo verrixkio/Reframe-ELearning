202336b01d047d145ee6f508f37cb8ad787ed176
Thomas Hulsmans <verrixkio@gmail.com>
2020-02-26 18:58:55 -0700

    Added Sass to file as well as first DB call isntance

--- app/controllers/api/tests_controller.rb
+++ app/controllers/api/tests_controller.rb
@@ -3,4 +3,12 @@ class Api::TestsController < ApplicationController
     @User = User.all
     render json: @User
   end
+
+  def create
+    puts params, 'these are the params!'
+    # puts params[:trail_id].to_i, 'this is a numbeR!!'
+    # Comment.create(data: params[:data], name: params[:name], trails_id: params[:trail_id].to_i)
+  end
+
+
 end
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './App.css';
+import './css/application.css';
+// import './css/application.css';
 
 class App extends Component {
   constructor(props) {
