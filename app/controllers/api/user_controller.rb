7caa50cf642bc7ea83034eaddd7861309f488561
verrixkio <verrixkio@gmail.com>
2020-04-20 19:15:14 -0600

    Add Login functionality to check if a user exists in the database before logging in

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,7 +1,22 @@
 class Api::UserController < ApplicationController
+    
+
     def index
       @User = User.all
       render json: @User
+    end
+
+
+    def login
+      @user = User.find_by(email: params[:email])
+
+      if @user
+        puts "that user exists"
+      else
+        puts "that user does not exist"
+        head 503
+      end
+
     end
 
     def create
--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,17 @@ Rails.application.routes.draw do
     get '/data', to: 'login#index'
 
     post '/user/new', to: 'user#create'
+
+    post '/login', to: 'user#login'
     
     resources :user, :reading, :course, :segment, :activity, :skill, :concept, :activitycompletion, :readingcompletion
 
 
   end
+
+  resources :users, only: [:new, :create]
+
+
 
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
   !request.xhr? && request.format.html?
